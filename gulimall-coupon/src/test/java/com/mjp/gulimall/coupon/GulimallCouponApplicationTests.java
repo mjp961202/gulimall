0967a3541c9b6f93e68260c8878385a87b71139e
minjianpeng <18366603006@139.com>
2021-04-27 17:15:59 +0800

    创建模块

---
+++ gulimall-coupon/src/test/java/com/mjp/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjp.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/mjp/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjp.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
