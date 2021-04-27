5c2c87b0a28b6e76f7fa2e67d7a0bb62db3de927
minjianpeng <18366603006@139.com>
2021-04-27 16:27:32 +0800

    创建项目

---
+++ src/test/java/com/mjp/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjp.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
