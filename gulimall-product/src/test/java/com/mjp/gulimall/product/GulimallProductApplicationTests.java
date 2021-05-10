ddc9f2a27ed6275243d210a3dfb4616fd13b1dd2
minjianpeng <18366603006@139.com>
2021-05-10 16:43:07 +0800

    创建模块

--- gulimall-product/src/test/java/com/mjp/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/mjp/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,31 @@
 package com.mjp.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.mjp.gulimall.product.dao.BrandDao;
+import com.mjp.gulimall.product.service.BrandService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    private BrandService brandService;
+
+    @Resource
+    private BrandDao brandDao;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+//        BrandEntity brandEntity=new BrandEntity();
+//        brandEntity.setName("测试");
+//        brandService.save(brandEntity);
+        brandDao.deleteById(6);
     }
 
 }
--- gulimall-ware/src/main/java/com/mjp/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/mjp/gulimall/ware/GulimallWareApplication.java
@@ -1,8 +1,10 @@
 package com.mjp.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.mjp.gulimall.ware.dao")
 @SpringBootApplication
 public class GulimallWareApplication {
 
